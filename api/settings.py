466034da97f45391ce221f9eed4850b1fcbb9ddd
Dev Raj <rehansingh.4522@gmail.com>
2022-10-05 02:08:27 +0530

    db connected

--- api/settings.py
+++ api/settings.py
@@ -85,22 +85,24 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': config('DB_NAME'),
+       'USER': config('DB_USER'),
+       'PASSWORD': config('DB_PASS'),
+       'HOST': 'dpg-ccu96osgqg4a928fc34g-a.singapore-postgres.render.com',
+    #    'HOST': config('DB_HOST'),
+       'PORT': config('DB_PORT'),
+   }
+}
+# 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'USER': 'mydatabaseuser',
-#         'NAME': 'mydatabase',
-#         'TEST': {
-#             'NAME': 'mytestdatabase',
-#         },
-#     },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
